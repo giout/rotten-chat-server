5f84599a5d48dd14861eabfd5899b3bbca19c720
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-09 20:03:22 -0400

    feat: define events

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,6 @@
 import { Server as SocketServer } from 'socket.io'
 import { Server } from 'http'
+import { Message } from '../types/messages.type'
 
 export const config = (srv: Server) => {
     const io = new SocketServer(srv, {
@@ -10,5 +11,19 @@ export const config = (srv: Server) => {
 
     io.on('connection', (socket) => {
         // events...
+        socket.on('enterRoom', (roomId) => {
+            // join room
+            // return all room messages in Message[] type
+        })
+
+        socket.on('sendMessage', (message: Message) => {            
+            const { roomId } = message
+            // save message in database
+            // emit a receiveMessage event to roomId room in which the message is the body 
+        })
+
+        socket.on('leaveRoom', () => {
+            // leave room
+        })
     })
 }
