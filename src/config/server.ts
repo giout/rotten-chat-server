d68414a02bac6c37a3e280bf4bb53bcb5ea284a3
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-10 19:21:08 -0400

    fix: enable cors to everyone in web service

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,9 +2,12 @@ import 'dotenv/config'
 import http from 'http'
 import express from 'express'
 import { config } from './socket'
+import cors from 'cors'
 
 // creating web server
 const app = express()
+app.use(cors())
+
 const srv = http.createServer(app)
 config(srv)
 
